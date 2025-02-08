f54ce071d8b7120859ba25dfe3cdafc61b9b37c0
dinar wijaksono <dinarwijaksono11@gmail.com>
2025-02-08 13:14:55 +0700

    setup laravel

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
+
+abstract class TestCase extends BaseTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        config(['database.default' => 'mysql-test']);
+
+        DB::delete('delete from users');
+        DB::delete('delete from sessions');
+        DB::delete('delete from password_reset_tokens');
+    }
+}
