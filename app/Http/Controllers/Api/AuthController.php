dc2407a1b989d5e0665e6472ad849ed2e29dc47e
dinar wijaksono <dinarwijaksono11@gmail.com>
2025-02-08 18:12:07 +0700

    Feature: (http request) menabhakan fitur login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Service\UserService;
 use Illuminate\Http\Request;
@@ -59,5 +60,49 @@ class AuthController extends Controller
                 'email' => $register->email
             ]
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), (new LoginRequest())->rules());
+
+        if ($validator->fails()) {
+            Log::warning('login failed', [
+                'email' => $request->email,
+                'message' => "validad error",
+                'errors' => $validator->errors()
+            ]);
+
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $result = $this->userService->loginForApi($request->email, $request->password);
+
+        if (!$result) {
+            Log::warning('login failed', [
+                'email' => $request->email,
+                'message' => "validad password error",
+                'errors' => $validator->errors()
+            ]);
+
+            return response()->json([
+                'errors' => [
+                    'general' => 'Email atau password salah.'
+                ]
+            ], 400);
+        }
+
+        Log::info('Login success', ['email' => $request->email]);
+
+        return response()->json([
+            'data' => [
+                'api_token' => $result->token,
+                'expired_token' => $result->expired_at,
+                'name' => $result->name,
+                'email' => $result->email
+            ]
+        ], 200);
     }
 }
--- tests/Feature/Http/Controllers/Api/AuthControllerTest.php
+++ tests/Feature/Http/Controllers/Api/AuthControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Http\Controllers\Api;
 
 use App\Models\User;
+use Database\Seeders\CreateUserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
@@ -61,5 +62,58 @@ class AuthControllerTest extends TestCase
         $response->assertStatus(400);
         $response->assertJsonStructure(['errors' => ['general']]);
         $response->assertJsonPath('errors.general', "Email tidak tersedia.");
+    }
+
+    public function test_login_return_success()
+    {
+        $this->seed(CreateUserSeeder::class);
+        $user = User::first();
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => 'rahasia1234'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['data' => ['api_token', 'expired_token', 'name', 'email']]);
+        $response->assertJsonPath('data.name', $user->name);
+    }
+
+    public function test_login_return_validate_error()
+    {
+        $response = $this->post('/api/login', [
+            'email' => '',
+            'password' => ''
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['email', 'password']]);
+    }
+
+    public function test_login_return_validate_email_error()
+    {
+        $response = $this->post('/api/login', [
+            'email' => 'emailIsWrong@gmail.com',
+            'password' => 'rahasia1234'
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJsonStructure(['errors' => ['general']]);
+        $response->assertJsonPath('errors.general', "Email atau password salah.");
+    }
+
+    public function test_login_return_error_password_is_wrong()
+    {
+        $this->seed(CreateUserSeeder::class);
+        $user = User::first();
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => 'this-password-wrong'
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJsonStructure(['errors' => ['general']]);
+        $response->assertJsonPath('errors.general', "Email atau password salah.");
     }
 }
