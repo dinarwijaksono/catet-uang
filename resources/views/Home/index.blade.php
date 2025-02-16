0a697f01d7d523f2fecd93b794b0f9c3384863d4
dinar wijaksono <dinarwijaksono11@gmail.com>
2025-02-16 19:28:54 +0700

    setup tailwind & daisyUi

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="business">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ env('APP_NAME') }}</title>
+    <link rel="stylesheet" href="/tailwind/style.css">
+</head>
+
+<body>
+
+    <button class="btn btn-primary btn-sm">kirim</button>
+
+</body>
+
+</html>
